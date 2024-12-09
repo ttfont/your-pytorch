03d0976437324b1b47aaba3a3fc182c91bdc933b
ttfont <ttfont>
2024-12-09 11:37:44 +0800

    变量 Variable 和 Tensor.py 02

--- 变量 Variable 和 Tensor.py
+++ 变量 Variable 和 Tensor.py
@@ -67,6 +67,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('变量 (Variable)')
+    print_hi('变量 Variable 和 Tensor')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
